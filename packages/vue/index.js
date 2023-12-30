96eb15565a1fd2ddd12ce73dcd1098f11aea48de
Lay <brownsugar.idv.tw@gmail.com>
2023-12-31 03:17:04 +0800

    feat(vue): add rule for vue/attributes-order

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -6,4 +6,24 @@ module.exports = {
     '@brownsugar/typescript',
     'plugin:vue/vue3-recommended',
   ],
+  rules: {
+    'vue/attributes-order': ['error', {
+      order: [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'GLOBAL',
+        'UNIQUE',
+        'SLOT',
+        'TWO_WAY_BINDING',
+        'OTHER_DIRECTIVES',
+        'CONTENT',
+        'ATTR_DYNAMIC',
+        'ATTR_STATIC',
+        'ATTR_SHORTHAND_BOOL',
+        'EVENTS',
+      ],
+    }],
+  },
 }
